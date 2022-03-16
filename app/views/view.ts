cdcfe943292125e9fd3bf9f586aa5e58bd8876b2
Bernado <bernadoafonso9734@gmail.com>
2022-03-16 17:02:52 -0300

    Classe view criada para ser reultilizada

---
+++ app/views/view.ts
@@ -0,0 +1,16 @@
+//criando a classe view para ser reultilizado nas outras views criadas
+export class View<T>{
+
+    protected elemento: HTMLElement;
+    constructor(seletor: string){
+        this.elemento = document.querySelector(seletor);
+    }
+
+    update(model: T): void{
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+    template(model: T): string{
+        throw Error("Classe filha precisa implementar o método template!");
+    }
+}
