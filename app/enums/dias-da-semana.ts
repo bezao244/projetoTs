de111a415c22cdda503d554e1755186bc98261d1
Bernado <bernadoafonso9734@gmail.com>
2022-03-22 17:14:35 -0300

    Criando uma enum de dias da semana

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,10 @@
+//criando enum para poder usar em qualquer arquvio do projeto
+export enum DiasDaSemana{
+    DOMINGO = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 4,
+    SEXTA = 5,
+    SABADO = 6
+}
